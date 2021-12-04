55c46957cc91e8e6e34c16ec5358d3f340c236ab
Siddharth Bhat <siddu.druid@gmail.com>
2021-12-05 01:38:12 +0530

    wip: refactoring to make instruction generation much faster

--- hoopl/rewrites.py
+++ hoopl/rewrites.py
@@ -51,8 +51,10 @@ shr = op2("shr")
 # sequence of instructions
 # TODO: change representation to have 'out value'
 class Program:                 
-    def __init__(self, name, insts=[]):
+    def __init__(self, name, ninputs=0, outreg=None, insts=[]):
         self.name = name
+        self.ninputs = ninputs
+        self.outreg = outreg
         self.insts = insts
         pass
 
@@ -61,10 +63,11 @@ class Program:
 
     def __str__(self):
         out = ""
-        out += "***" + self.name + "***" + "\n"
+        out += "***" + self.name + f"(ninputs={self.ninputs})" + "***" + "\n"
         # out += "-" * len(self.name) + "\n"
         for inst in self.insts:
             out += str(inst[0]) + " = " + inst[1] +  "(" + ", ".join(map(str, inst[2:])) + ")" + "\n"
+        out += f"return {self.outreg}\n"
         return out
     __repr__ = __str__
 
@@ -115,10 +118,6 @@ def symbolic_inst(env, inst):
     kind = inst[1]
     rhs = [symbolic_operand(env, rand) for rand in inst[2:]]
 
-    if kind == "return":
-        new_env = {}  # clear environment to keep only return value
-        new_env[lhs] = rhs[0]
-        return new_env
 
     if kind == "add":
         env[lhs] = rhs[0] + rhs[1]
@@ -129,21 +128,20 @@ def symbolic_inst(env, inst):
     return env
 
 def symbolic_program(p: Program):
-    env = {}
+    env = { f"reg{i}": Int(f"reg{i}") for i in range(p.ninputs) }
     for inst in p.insts:
         env = symbolic_inst(env, inst)
-    return env # return environment
+    return env[p.outreg] # return environment
 
 # returns if two programs are symbolically equivalent
 def symbolic_program_is_equiv(p: Program, q: Program):
-    penv = symbolic_program(p); qenv = symbolic_program(q)
-    outp = penv["out"]
-    outq = qenv["out"]
+    outp = symbolic_program(p); outq = symbolic_program(q)
     query = outp == outq
 
-    max_input_ix = max(get_largest_input_ix_program(p), get_largest_input_ix_program(q))
-    for i in range(max_input_ix):
-        query = ForAll(Int(f"in{i}"), query)
+    if p.ninputs != q.ninputs: return False
+
+    for i in range(p.ninputs):
+        query = ForAll(Int(f"reg{i}"), query)
     s = smt(query)
     is_eq = s.check() == sat
 
@@ -162,8 +160,6 @@ def cost_inst(inst):
         return 1
     elif kind == "mul":
         return 4
-    elif kind == "return":
-        return 0 # return is zero cost
     else:
         raise RuntimeError(f"unknown instruction |{inst}|")
 
@@ -171,156 +167,118 @@ def cost_program(program):
     """Make sure cost is >= 1 so that when we take log, we get 0 as minimum"""
     return 1 + sum([cost_inst(inst) for inst in program.insts])
 
-def rand_operand(totinputs: int):
-   kind = random.choice(["new-input", "old-input", "constant"])
-   if kind == "new-input":
-       return (f"in{totinputs}", totinputs+1)
-   elif kind == "old-input":
-       ix = random.randint(0, totinputs-1)
-       return (f"in{ix}", totinputs)
+def rand_operand(nregs: int):
+   kind = random.choice(["reg", "constant"])
+   if kind == "reg":
+       ix = random.randint(0, nregs-1)
+       return (f"reg{ix}", nregs)
    elif kind == "constant":
        const = random.randint(-2, -2)
-       return (const, totinputs)
+       return (const, nregs)
    else:
        raise RuntimeError(f"unknown kind of random operand: |{kind}|")
 
 
 # TODO: create a class to create a random instruction?
-def rand_inst(totinputs: int):
+def rand_inst(lhs: str, nregs: int):
     """
-    totinputs: number of existing input variables
+    nregs: number of existing registers
     """
     kind = random.choice(["add", "sub", "mul"])
     if kind in ["add", "sub", "mul"]:
-        (lhs, totinputs) = (f"in{totinputs}", totinputs+1)
-        (rhs1, totinputs) = rand_operand(totinputs)
-        (rhs2, totinputs) = rand_operand(totinputs)
+        (rhs1, nregs) = rand_operand(nregs)
+        (rhs2, nregs) = rand_operand(nregs)
         inst = (lhs, kind, rhs1, rhs2)
-        return (inst , totinputs)
+        return (inst , nregs)
     else:
         raise RuntimeError(f"unknown instruction kind |{kind}|")
 
-def rand_program(name, ninsts):
-    totinputs = 0
+def rand_program(name, ninputs, ninsts):
+    nregs = ninputs
     insts = []
     for _ in range(ninsts):
-        (inst, totinputs) = rand_inst(totinputs)
+        (lhs, nregs) = (f"reg{nregs}", nregs+1)
+        (inst, nregs) = rand_inst(lhs, nregs)
         insts.append(inst)
 
-    ix = random.randint(0, totinputs-1); rhs = f"in{ix}"
-    retinst = ("out", "return", rhs)
-    insts.append(retinst)
-    return Program(name, insts)
+    outix = random.randint(0, nregs-1)
+    return Program(name, ninputs=ninputs, outreg=f"reg{outix}", insts=insts)
 
-def get_largest_input_ix_inst(inst):
+def get_largest_used_reg_inst(inst):
     """
-    Get index (N) of the largest "inN" value used by the instruction.
-    Returns 0 minimum
+    Get index (the value N) of the largest "regN" value used by the instruction.
+    Returns -1 minimum
     """
     lhs = inst[0]
     name = inst[1]
     rhss = inst[2:]
-    out = 0
-    if lhs.startswith("in"):
-        out = max(out, int(lhs.split("in")[1]))
+    out = -1
+    if lhs.startswith("reg"):
+        out = max(out, int(lhs.split("reg")[1]))
     for rhs in rhss:
         if not isinstance(rhs, str): 
             assert isinstance(rhs, int) # is a constant arg.
             continue # continue
         assert isinstance(rhs, str)
-        if rhs.startswith("in"):
-            out = max(out, int(rhs.split("in")[1]))
+        if rhs.startswith("reg"):
+            out = max(out, int(rhs.split("reg")[1]))
     return out
 
-def get_largest_input_ix_program(p):
+def get_num_regs(p):
     """
-    Get index(N) of the largest "inN" value used by the program.
+    Get index(the value N) of the largest "regN" value used by the program.
     Returns 0 minimum
     """
-    out = 0
+    out = -1
     for inst in p.insts:
-        out = max(out, 1 + get_largest_input_ix_inst(inst))
-    return out
+        out = max(out, get_largest_used_reg_inst(inst))
+    return out + 1
 
 
-def reindex_program_insts(p):
-    """
-    Reindex a program's instruction names to be in the set [0..|num_names_needed|]
-    """
-
-    nname = 0
-    reindexed_names = {}
-    q = Program(p.name, insts=[])
+def is_reg_used(p:Program, reg:str):
+    if p.outreg == reg: return True
     for inst in p.insts:
         lhs = inst[0]
-        kind = inst[1]
-        rhss = list(inst[2:]) # convert to list for mutation
-
-
-        # generate new name for rhs
-        for i in range(len(rhss)):
-            if rhss[i] not in reindexed_names:
-                reindexed_names[rhss[i]]= f"in{nname}"
-                nname += 1
-            rhss[i] = reindexed_names[rhss[i]]
-
-        # generate new name for lhs
-        if lhs != "out":
-            if lhs not in reindexed_names:
-                reindexed_names[lhs] = f"in{nname}"
-                nname += 1
-            lhs = reindexed_names[lhs]
-
-        q.insts.append(tuple([lhs, kind] + rhss))
-
-    return q
+        rhss = inst[2:]
+        if lhs == reg: return True
+        if reg in rhss: return True
+    return False
         
 
 # Mutate a program.
 def mutate_program(p):
     p = copy.deepcopy(p)
     vars_so_far = set()
-    mutation_type = random.choice(["edit", "delete", "insert"])
-    # don't delete from program with only `return`.
-    if len(p.insts) == 2:
-        mutation_type = random.choice(["edit", "insert"])
+    mutation_type = random.choice(["edit", "delete", "insert", "changeret"])
     # print("mutation_type: %s" % mutation_type)
     if mutation_type == "edit":
-        ix_to_edit = random.randint(0, len(p.insts)-2) # index: will insert at [location+eps, location+1)
-        (inst, totinputs) = rand_inst(get_largest_input_ix_program(p) + 1)
+        ix_to_edit = random.randint(0, len(p.insts)-1) # index: will insert at [location+eps, location+1)
+        inst = p.insts[ix_to_edit]; lhs = inst[0]
+        (inst, nregs) = rand_inst(lhs=lhs, nregs=get_num_regs(p))
         p.insts[ix_to_edit] = inst
-        p = reindex_program_insts(p)
         return p
     elif mutation_type == "delete":
-        # currently: does not delete the return instruction!
-        ix = random.randint(0, len(p.insts)-2)
+        if len(p.insts) == 1: return p # cannot delete empty program
+        ix = random.randint(0, len(p.insts)-1)
+        inst = p.insts[ix]; lhs = inst[0]
+        if is_reg_used(p, lhs): return p # cannot delete register in use
         del p.insts[ix]
         return p
     elif mutation_type == "insert":
-        new_inst, totinputs = rand_inst(get_largest_input_ix_program(p) + 1)
-        # TODO: allow changing the return instruction to be the new instruction
-        ix_to_insert = random.randint(0, len(p.insts)) # index: will insert at [location+eps, location+1)
-        if ix_to_insert == len(p.insts): # we are adding an instruction to the end
-            del p.insts[-1] # delete return instruction
-            p.insts.append(new_inst) # add new instruction
-            p.insts.append(('out', 'return', new_inst[0])) # create new return instruction
-        else: # not adding last instruction
-            p.insts.insert(ix_to_insert, new_inst)
-        p = reindex_program_insts(p)
+        # index: will insert such that q[<ix] = p[<ix] | q[ix] = new | q[>ix'] = p[ix'-1]
+        ix_to_insert = random.randint(0, len(p.insts)) 
+        nregs = p.ninputs
+        for inst in p.insts[ix_to_insert]: 
+            # TODO: number of registers is such a pain. Keep a set of strings?
+            raise RuntimeError("unimplemented")
+        lhs = f"reg{nregs}"; nregs += 1
+        new_inst, nregs = rand_inst(lhs, nregs)
+        p.insts.append(new_inst) # add new instruction
         return p
-
-def get_program_inputs(p):
-    defs = set()
-    inputs = set()
-    for inst in p.insts:
-        lhs = inst[0]
-        kind = inst[1]
-        rhss = inst[2:]
-        for rhs in rhss:
-            if not isinstance(rhs, str): continue
-            if rhs not in defs: inputs.add(rhs)
-        defs.add(lhs)
-    return inputs
+    elif mutation_type == "changeret":
+        ix = random.randint(0, get_num_regs(p)-1)
+        p.outreg = f"reg{ix}"
+        return p
 
 
 def run_operand_concrete(operand, env):
@@ -341,8 +299,6 @@ def run_inst_concrete(inst, env):
         env[lhs] = rhss[0] - rhss[1]
     elif kind == "mul":
         env[lhs] = rhss[0] * rhss[1]
-    elif kind == "return":
-        env[lhs] = rhss[0]
     else:
         raise RuntimeError(f"unknown instruction |{inst}|")
 
@@ -350,14 +306,19 @@ def run_program_concrete(p, env):
     """
     Run program on input dictionary env: inx -> value
     """
+    print(p)
+    env = copy.deepcopy(env)
+    for i in range(p.ninputs):
+        assert f"reg{i}" in env
+
     for inst in p.insts:
         run_inst_concrete(inst, env)
-    return env["out"]
+    return env[p.outreg]
 
 def run_stoke():
     ninsts = 4
-    p = rand_program("rand-0", ninsts)
-    p_inputs = get_program_inputs(p)
+    ninputs = 2
+    p = rand_program("rand-0", ninputs, ninsts)
     log_score_p = - math.log(cost_program(p)) # score = log(1/cost)
     q_best = p; log_score_best = log_score_p
     q = p
@@ -369,20 +330,16 @@ def run_stoke():
             q = mutate_program(q)
         log_score_q = - math.log(cost_program(q)) # score = log(1/cost)
 
-        q_inputs = get_program_inputs(q)
-        # print(f"inputs: |{p_inputs}| == |{q_inputs}|")
-        # TODO: this is really stupid, I should just fix the number of inputs in the Program.
-        if p_inputs == q_inputs:
+        if p.ninputs == q.ninputs:
             N_CONCRETE_RUNS = 10
             all_concrete_runs_matched = True
             for i in range(N_CONCRETE_RUNS):
-                init_env = { inp :  random.randint(-3, 3) for inp in p_inputs }
+                init_env = { f"reg{i}" :  random.randint(-3, 3) for i in range(p.ninputs) }
                 if run_program_concrete(p, init_env) == run_program_concrete(q, init_env):
                     log_score_q += 1 # each correct matching output 
                 else:
                     all_concrete_runs_matched = False
 
-            print(f"p: {symbolic_program(p)['out']} | q: {symbolic_program(q)['out']}")
             # run symbolic equivalence if concrete runs all match.
             if all_concrete_runs_matched and symbolic_program_is_equiv(p, q):
                 log_score_q += 10 # correctness is extremely important.
